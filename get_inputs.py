115e0bc6ce75a70dbd9666381c471a427b54d170
Marcel Schubert <s8mascub@stud.uni-saarland.de>
2023-12-02 12:44:01 +0100

    Added get_inputs

---
+++ get_inputs.py
@@ -0,0 +1,39 @@
+import requests
+import os
+from argparse import ArgumentParser
+from configparser import ConfigParser
+
+from cookie import COOKIE
+
+configparser = ConfigParser()
+configparser.read("config.ini")
+
+YEAR = configparser.getint("Basic", "Year")
+COOKIE = configparser.get("Basic", "Cookie")
+
+argparser = ArgumentParser()
+argparser.add_argument(
+    "day", type=int, help="download the input file for this day")
+args = argparser.parse_args()
+
+
+def get_url(day: int, year: int = YEAR) -> str:
+    return f"https://adventofcode.com/{year}/day/{day}/input"
+
+
+def download(day: int, year: int = YEAR):
+    url = get_url(day, year)
+    response = requests.get(url, cookies={"session": COOKIE})
+    print(f"Downloading from {url} using cookie {COOKIE}.")
+    if response.status_code != 200:
+        print("Could not download input file. GET request with status code:",
+              response.status_code)
+        return
+    os.makedirs("input", exist_ok=True)
+    with open(f"input/day{day:02d}.txt", "wb") as f:
+        f.write(response.content)
+        print(f"Downloaded to {f.name}")
+
+
+if __name__ == "__main__":
+    download(args.day)
