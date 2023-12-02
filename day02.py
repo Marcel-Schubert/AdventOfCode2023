0caed931d99c3317fae48ef578e4863aa4af330d
Marcel Schubert <s8mascub@stud.uni-saarland.de>
2023-12-02 16:11:14 +0100

    Day 2

---
+++ day02.py
@@ -0,0 +1,46 @@
+import re
+from math import prod
+
+with open("input/day02.txt", "r") as f:
+    lines = f.readlines()
+
+color_max = {'red': 12, 'green': 13, 'blue': 14}
+
+
+def build_game_dicts(game_str: str) -> list[dict[str, int]]:
+    game_dicts: list[dict[str, int]] = []
+    game: list[str] = game_str.split("; ")
+    for reveal in game:
+        reveal = reveal.split(", ")
+        d = {}
+        for color_count in reveal:
+            count, color = color_count.split()
+            d[color] = int(count)
+        game_dicts.append(d)
+    return game_dicts
+
+
+def is_possible(game: list[dict[str, int]]) -> bool:
+    for reveal in game:
+        for color, count in reveal.items():
+            if count > color_max[color]:
+                return False
+    return True
+
+
+def get_power(game: list[dict[str, int]]) -> int:
+    return prod(max(reveal[color] for reveal in game if color in reveal) for color in color_max)
+
+
+games = dict()
+for line in lines:
+    game_id, game = re.match(r"Game (\d+): (.*)", line).groups()
+    game_id = int(game_id)
+    games[game_id] = build_game_dicts(game)
+
+
+part1 = sum(game_id for game_id, game in games.items() if is_possible(game))
+part2 = sum(get_power(game) for _, game in games.items())
+
+print("Part 1:", part1)
+print("Part 2:", part2)
