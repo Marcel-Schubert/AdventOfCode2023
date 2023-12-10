f62430279d82007441932948d4bf47cd932af0e9
Marcel Schubert <s8mascub@stud.uni-saarland.de>
2023-12-10 18:36:13 +0100

    Day10

--- day10.py
+++ day10.py
@@ -12,6 +12,28 @@ def find_start_pos() -> Pos:
     raise (ValueError("No starting position found."))
 
 
+def replace_s_pos(pos: Pos):
+    row, col = pos
+    assert lines[row][col] == "S"
+    possibilities = {x for x in "|-LJF7"}
+    # below connects
+    if row + 1 < len(lines) and lines[row + 1][col] in "|LJ":
+        possibilities &= {x for x in "|F7"}
+    # left connects
+    if 0 <= col - 1 and lines[row][col - 1] in "-LF":
+        possibilities &= {x for x in "-7J"}
+    # above connects
+    if 0 <= row - 1 and lines[row - 1][col] in "|7F":
+        possibilities &= {x for x in "|LJ"}
+    # right connects
+    if col + 1 < len(lines[row]) and lines[row][col + 1] in "-J7":
+        possibilities &= {x for x in "-LF"}
+    if len(possibilities) != 1:
+        raise (ValueError("Start position does not have exactly 2 possible neighbours."))
+    replacement = possibilities.pop()
+    lines[row] = lines[row][:col] + replacement + lines[row][col + 1 :]
+
+
 def next_pos(pos: Pos) -> tuple[Pos, Pos]:
     row, col = pos
     symbol = lines[row][col]
@@ -31,21 +53,13 @@ def next_pos(pos: Pos) -> tuple[Pos, Pos]:
         case ".":
             raise ValueError("It should be possible to reach a . position.")
         case "S":
-            positions: list[Pos] = []
-            if row + 1 < len(lines) and lines[row + 1][col] in "|LJ":
-                positions.append((row + 1, col))
-            if 0 <= col - 1 and lines[row][col - 1] in "-LF":
-                positions.append((row, col - 1))
-            if 0 <= row - 1 and lines[row - 1][col] in "|7F":
-                positions.append((row - 1, col))
-            if col + 1 < len(lines[row]) and lines[row][col + 1] in "-J7":
-                positions.append((row, col + 1))
-            if len(positions) != 2:
-                raise (ValueError("Start position does not have exactly 2 possible neighbours."))
-            return positions[0], positions[1]
+            replace_s_pos(pos)
+            return next_pos(pos)
 
 
-open: list[Pos] = [find_start_pos()]
+start_pos = find_start_pos()
+replace_s_pos(start_pos)
+open: list[Pos] = [start_pos]
 closed: set[Pos] = set()
 
 # BFS
@@ -58,4 +72,15 @@ while open:
 
 part1 = len(closed) // 2
 
+part2 = 0
+inside = False
+for row, line in enumerate(lines):
+    for col, symbol in enumerate(line):
+        if (row, col) in closed:
+            if symbol in "|F7":
+                inside = not inside
+        elif inside:
+            part2 += 1
+
 print("Part 1:", part1)
+print("Part 2:", part2)
